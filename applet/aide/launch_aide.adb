2de90f12a7850924941031b796701fa7b2cf1f81
Rod Kay <rodakay@internode.on.net>
2017-05-07 13:13:13 +1000

    aide ~ open/close the adam factory on applet launch/terminate

--- applet/aide/launch_aide.adb
+++ applet/aide/launch_aide.adb
@@ -1,10 +1,13 @@
 with
-     aIDE;
+     aIDE,
+     AdaM.Factory;
 
 procedure launch_AIDE
 is
 begin
+   AdaM.Factory.open;
    aIDE.start;
    aIDE.stop;
+   AdaM.Factory.close;
 end launch_AIDE;
 
